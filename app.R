d64c12ea9370e53dd895ef6d35759e6e955ed183
Ivan Rodriguez <drodriguez@cimat.mx>
2022-05-20 11:13:21 -0500

    Titulo

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ ui <- fluidPage(
     # Application title
   
     #GIT - EDITAR TITULO DE LA APLICACION ###################################
-    titlePanel("Iris Database: Classification with Random Forest (versión 2.2.4 j)"),
+    titlePanel("Iris Database: Classification with Random Forest (versión 2.2.4 jjjj)"),
     #########################################################################
 
     # Sidebar with a slider input for number of bins 
